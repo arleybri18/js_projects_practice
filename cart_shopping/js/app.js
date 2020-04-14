13913e09203f040710cd0bc40c30dd1a7e25b378
Yony Brinez <ybrinez@picap.co>
2020-04-13 21:37:55 -0500

    functions for add and delete courses in table, and local storage

--- cart_shopping/js/app.js
+++ cart_shopping/js/app.js
@@ -2,7 +2,7 @@
 const carrito = document.getElementById('carrito');
 const cursos = document.getElementById('lista-cursos');
 const listaCursos = document.querySelector('#lista-carrito tbody');
-
+const vaciarCarritoBtn = document.getElementById("vaciar-carrito");
 
 // Listeners
 cargarEventListeners();
@@ -10,6 +10,15 @@ cargarEventListeners();
 function cargarEventListeners(){
     // diapara cuando se da clic en "Agregar Carrito"
     cursos.addEventListener('click', cargarCurso);
+
+    // Dispara cuando se elimina un curso del carrito
+    carrito.addEventListener('click', eliminarCurso);
+
+    // Vaciar todo el carrito
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+    // Cargar elementos del carrito
+    document.addEventListener('DOMContentLoaded', cargarCursosCarrito);
 
 }
 
@@ -41,6 +50,26 @@ function leerDatosCurso(curso){
 
 // muestra la informacion del curso seleccionado en el carrito
 function insertarCarrito(infoCurso){
+    let cursosStorage;
+    let idCursos;
+    // obtener los cursos del local storage
+    cursosStorage = obtenerCursosLocalStorage();
+    // Traer los id de los cursos que ya estan en local storage
+    idCursos = cursosStorage.map((c) => c.id);
+   
+    // si no esta guardado, mandarlo pintar e insertarlo en local storage
+    if(!idCursos.includes(infoCurso.id))
+    {
+        crearRowCursos(infoCurso);
+        guardarCursoLocalStorage(infoCurso);
+    }
+
+}
+
+
+
+// crear fila agregando el curso
+function crearRowCursos(infoCurso){
     const row = document.createElement('tr');
     row.innerHTML = `
         <td><img src="${infoCurso.imagen}"></td>
@@ -50,5 +79,77 @@ function insertarCarrito(infoCurso){
     `
     ;
     listaCursos.appendChild(row);
+}
 
+// elimina un curso seleccionado
+function eliminarCurso(e){
+    e.preventDefault();
+    let cursoId;
+    if(e.target.classList.contains("borrar-curso")){
+        cursoId = e.target.getAttribute("data-id");
+        e.target.parentElement.parentElement.remove();
+    }
+    eliminarCursoStorage(cursoId);
+}
+
+// vaciar todo el carrito
+
+function vaciarCarrito(e){
+    e.preventDefault();
+    // fomra rapida y recomendada iterar sobre los hijos de tbody
+    while(listaCursos.firstChild){
+        listaCursos.removeChild(listaCursos.firstChild);
+    }
+    localStorage.setItem('cart', JSON.stringify([]));
+    // prevenir saltos en la pagina, hacer un return false
+    return false;
+}
+
+// guarda en local storage
+function guardarCursoLocalStorage(infoCurso){
+    let cart;
+
+    cart = obtenerCursosLocalStorage();
+    cart.push(infoCurso);
+    // Guardar en localstorage pero en string
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+// Valida si hay elementos en local storage y retorna arreglo
+function obtenerCursosLocalStorage(){
+    let cart;
+    // Revisar los valores de local storage
+    if(localStorage.getItem('cart') === null){
+        // crear array vacio
+        cart = [];
+    } else {
+        // obtener el array que esta como string en la variable de Local Storage
+        cart = JSON.parse(localStorage.getItem('cart'));
+    }
+    return cart;
+}
+
+// cargar cursos del local storage y llenar el carrito
+
+function cargarCursosCarrito(){
+    let cursosStorage;
+    cursosStorage = obtenerCursosLocalStorage();
+    cursosStorage.forEach(function(curso) {
+        // Crear row
+        crearRowCursos(curso);
+
+    })
+}
+
+// eliminar cursos de local storage
+function eliminarCursoStorage(cursoId){
+        let cursosStorage;
+        cursosStorage = obtenerCursosLocalStorage();
+        cursosStorage.forEach(function(curso, index){
+            if(cursoId === curso.id){
+                cursosStorage.splice(index, 1);
+            }
+        });
+        // Guardar en localstorage pero en string
+    localStorage.setItem('cart', JSON.stringify(cursosStorage));
 }
